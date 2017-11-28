dec86831ef68a425deb0efaed581a81eaf01bda4
yvester <yves.l.tanguy@gmail.com>
2017-11-28 14:19:33 +0100

    Add files via upload

--- source/Process.bat
+++ source/Process.bat
@@ -1,3 +1,5 @@
+@echo off
+
 REM Script Ramenerlechat,
 REM ***************************************************
 REM Zips the project folder 
@@ -5,16 +7,15 @@ REM Generates the SHA on the remote machine
 REM Transfer the ZIP and SHA files to the local machine 
 REM ***************************************************
 
-@echo off
-
 REM Get the config parameters 
 call "%~dp0%..\config\Config.bat"
-echo ServerName =  %ServerName%
-echo PathName =  %PathName%
+REM echo ServerName =  %ServerName%
+REM echo PathName =  %PathName%
+REM echo filename = %~n1
 
 REM Get the name of the folder to zip 
-set /p ProjectName=<%~n1.zip
-echo ProjectName=%ProjectName%
+set ProjectName=%~n1
+REM echo ProjectName=%ProjectName%
 IF "%ProjectName%" == "" GOTO :MISSINGNAME
   
 REM Create the bash script to be executed on the remote server 
@@ -22,30 +23,34 @@ REM Create the bash script to be executed on the remote server
 echo cd /home/tibco
 echo mkdir -p ./ramenerlechat
 echo rm -f ./ramenerlechat/*
-echo cd ./ramenerlechat/
+echo cd %PathName%
+echo sleep 3s
 echo if [ ! -d %PathName%%ProjectName% ]; then
 echo   echo "Folder %ServerName%%PathName%%ProjectName% does not exist"  ^> %ProjectName%.sha
 echo exit
 echo fi
-echo zip -r %ProjectName%.zip %PathName%%ProjectName%
-echo md5sum %ProjectName%.zip ^> %ProjectName%.sha
-echo fi 
+echo zip -r /home/tibco/ramenerlechat/%ProjectName%.zip ./%ProjectName% 
+echo md5sum ./%ProjectName%.zip ^> /home/tibco/ramenerlechat/%ProjectName%.sha
+echo sleep 5s 
+echo fi
 ) > %TEMP%\remotescript.txt
 
+REM more %TEMP%\remotescript.txt
+
 cd %~dp1
+REM echo "DP13 %~dp1
 
 REM execute the bash script on the remote server
-echo "%ProgramFiles(x86)%"\Putty\putty.exe -ssh -l tibco -m %TEMP%\remotescript.txt %ServerName%
-"%ProgramFiles(x86)%"\Putty\putty.exe -ssh -l tibco -m %TEMP%\remotescript.txt %ServerName%
+REM echo "%ProgramFiles(x86)%"\Putty\putty.exe -ssh -l tibco -m %TEMP%\remotescript.txt %ServerName% 2>/dev/null
+"%ProgramFiles(x86)%"\Putty\putty.exe -ssh -l tibco -m %TEMP%\remotescript.txt %ServerName% 
 
 REM Copy the files from remote server to final folder
-echo "%ProgramFiles(x86)%"\pscp\pscp.exe -scp -unsafe tibco@%ServerName%:/home/tibco/ramenerlechat/* .
+REM echo "%ProgramFiles(x86)%"\pscp\pscp.exe -scp -unsafe tibco@%ServerName%:/home/tibco/ramenerlechat/* .
 "%ProgramFiles(x86)%"\pscp\pscp.exe -scp -unsafe tibco@%ServerName%:/home/tibco/ramenerlechat/* .
 
 REM Cleanup temp files
-del %TEMP%\remotescript.txt
+REM del %TEMP%\remotescript.txt
 GOTO :END
-
 
 :MISSINGNAME
 echo Missing Project Name
--- source/ProcessReg.bat
+++ source/ProcessReg.bat
@@ -1,6 +1,5 @@
 @echo off
 echo Set the registry key for ramenerlechat!
 echo %ProgramFiles(x86)%
-reg add HKEY_CLASSES_ROOT\7-Zip.zip\shell\RamenerLeChat\command /t REG_SZ /d "%ProgramFiles(x86)%\ramenerlechat\source\Process.bat %%1" /f
-reg add HKEY_CLASSES_ROOT\CompressedFolder\shell\RamenerLeChat\command /t REG_SZ /d "%ProgramFiles(x86)%\ramenerlechat\source\Process.bat %%1" /f
+Reg add HKEY_CLASSES_ROOT\7-Zip.zip\shell\RamenerLeChat\command /t REG_SZ /d "\"%ProgramFiles(x86)%\ramenerlechat\source\Process.bat\" ""%%1\" /f
 REM pause
